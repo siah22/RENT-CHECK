a8e0ed68b6c513fe9a8afe47a7485b7a9504333a
siah22 <simonsiah8@gmail.com>
2026-09-11 09:43:34 +0300

    Update root urls.py with properties routing and home redirect

--- PEPE/urls.py
+++ PEPE/urls.py
@@ -1,15 +1,21 @@
+from django.contrib import admin
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    # Admin interface
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
+    
+    # Properties application routes
     path('properties/', include('properties.urls')),
-    path('', include('engagement.urls')),
-    path('', include('core.urls')),
+    
+    # Redirect root URL ('/') directly to '/properties/'
+    path('', RedirectView.as_view(url='/properties/', permanent=False)),
 ]
 
+# Serve user-uploaded media files (property images) during development and testing
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
