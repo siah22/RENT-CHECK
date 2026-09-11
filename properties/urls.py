942855a7bebfda8c3f507b231dfbc82cc9937ba3
siah22 <simonsiah8@gmail.com>
2026-09-11 10:17:24 +0300

    Add /properties/new/ route to match navigation link

--- properties/urls.py
+++ properties/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.browse_properties, name='browse'),
     path('<int:pk>/', views.property_detail, name='detail'),
     path('create/', views.property_create, name='create'),
+    path('new/', views.property_create, name='new'),  # Handles clicks from "Create New Property"
 ]
